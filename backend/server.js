21c1f4c4b893ff5c233d8e8357b099f16184c98e
harshit goyal <harshit22122002@gmail.com>
2023-06-24 16:08:51 +0530

    second comit

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,17 @@ const dbConnect = require("./database/index");
 const { PORT } = require("./config/index");
 const router = require("./routes/index");
 const cookieParser = require('cookie-parser');
+const cors = require("cors");
 const errorHandler = require('./middleware/errorHandler.js')
-// const corsOptions = {
-//   credentials: true,
-//   origin: ["http://localhost:3000"],
-// };
+const corsOptions = {
+  credentials: true,
+  origin: ['http://localhost:3000'],
+};
 
 const app = express();
 app.use(cookieParser());
-app.use(express.json());
+app.use(cors(corsOptions));
+app.use(express.json({limit: '50mb'}));
 app.use(router);
 
 
